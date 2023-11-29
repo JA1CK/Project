5e6c300e519e6fe82d7066ddf73a50e22fa14e85
jainish <98240max11@gmail.com>
2023-11-29 10:26:37 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://jainishcadpatel:1Jaini$hMongoDB1@jpcluster.1mbmg.mongodb.net/sample_mflix"
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var app = express();
+var database = require("./config/database");
+var bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+var path = require("path");
+
+var port = process.env.PORT || 8000;
+app.use(bodyParser.urlencoded({ extended: "true" }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// serve static file from 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect(database.url);
+
+var Movies = require("./models/movies");
+
+// use handlebars as view engine
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+
+//get all employee data from db
+app.get("/", function (req, res) {
+  // use mongoose to get all todos in the database
+  Movies.find()
+    .exec()
+    .then((movies) => {
+      // send data
+      res.json(employees);
+    })
+    .catch((err) => {
+      // send the error
+      res.send(err);
+    });
+});
+
+app.listen(port);
+console.log("App listening on port : " + port);
